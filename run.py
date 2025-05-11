c6e47af66ac9f0d64dfd4d9da5d079060e4496a7
Philipp Seelos <seelosp@proton.me>
2025-05-11 22:45:36 +0200

    added zip for windows build

---
+++ run.py
@@ -0,0 +1,35 @@
+# run.py
+
+import os
+import sys
+import locale
+import gettext
+import signal
+from gi.repository import Gio
+
+VERSION = "0.2.0"
+
+# Paths relative to PyInstaller bundle
+base = os.path.abspath(os.path.dirname(sys.argv[0]))
+share_dir = os.path.join(base, "share")
+pkgdatadir = os.path.join(share_dir, "Stratum")
+localedir = os.path.join(share_dir, "locale")
+
+sys.path.insert(1, pkgdatadir)
+
+# Signals
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+# i18n
+locale.bindtextdomain("drucken3d", localedir)
+locale.textdomain("drucken3d")
+gettext.install("drucken3d", localedir)
+
+# Load GResource
+resource_path = os.path.join(pkgdatadir, "drucken3d.gresource")
+resource = Gio.Resource.load(resource_path)
+resource._register()
+
+# Run main app
+from drucken3d import main
+sys.exit(main.main(VERSION))
---
+++ stratum.spec
@@ -0,0 +1,46 @@
+# drucken3d.spec
+
+block_cipher = None
+
+a = Analysis(
+    ['run.py'],
+    pathex=['.'],
+    binaries=[],
+    datas=[
+        ('build-install/share/*', 'share'),
+    ],
+    hiddenimports=[],
+    hookspath=[],
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    [],
+    exclude_binaries=True,
+    name='drucken3d',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    console=False,
+)
+
+coll = COLLECT(
+    exe,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    name='drucken3d'
+)
